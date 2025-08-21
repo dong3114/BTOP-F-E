d6ebb988aeb481ee07cc54d6fd0d5265647cad75
wizdmoon <wizd.mooncre@gmail.com>
2025-08-21 12:56:40 +0900

    admin 페이지

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import MainLayout from './pages/mainlayout';
 import AppRoutes from './utils/Routes';
-import { ToastContainer } from 'react-toastify';
-import AdminMain from './pages/admin/AdminMain';
-import AdminDashboard from './pages/admin/AdminDashboard';
-import AdminUsers from './pages/admin/AdminUsers';
-import AdminBoards from './pages/admin/AdminBoards';
+
 
 export default function App() {
   return (
