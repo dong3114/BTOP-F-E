175da8bb6a279e6292319f217014fadcfe3e8b04
wizdmoon <wizd.mooncre@gmail.com>
2025-08-21 13:53:39 +0900

    admin css 수정

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function AdminDashboard() {
   return (
-    <div className=''>AdminDash55555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555</div>
+    <div>
+      <div className='title'>
+        <p>Dashboard</p>
+      </div>
+      <div>
+        <div>
+        <div className='search'>
+        </div>
+        </div>
+        <div className='tablebox'>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/admin/AdminUsers.jsx
+++ src/pages/admin/AdminUsers.jsx
@@ -2,7 +2,50 @@ import React from 'react'
 
 function AdminUsers() {
   return (
-    <div className=''>AdminUsers</div>
+    <div>
+      <div className='title'>
+        <p>회원 관리</p>
+      </div>
+      <div>
+        <div>
+        <div className='search'>
+          <input type='text' placeholder="title" name='searchText' id='searchText'></input>
+        </div>
+        </div>
+        <div className='tablebox'>
+        <table>
+          <colgroup>
+          <col width={"5%"}></col>
+          <col width={"10%"}></col>
+          <col width={"20%"}></col>
+          <col width={"20%"}></col>
+          <col width={"30%"}></col>
+          <col width={"15%"}></col>
+          </colgroup>
+					<thead>
+						<tr>
+									<th><input type='checkbox'></input></th>
+									<th>번호</th>
+									<th>아이디</th>
+									<th>이름</th>
+									<th>연락처</th>
+									<th>관리</th>
+								</tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td><input type='checkbox'></input></td>
+                  <td>2</td>
+                  <td>3</td>
+                  <td>4</td>
+                  <td>5</td>
+                  <td>6</td>
+                </tr>
+                </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   )
 }
 
